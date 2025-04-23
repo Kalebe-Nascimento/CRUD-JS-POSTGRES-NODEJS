0ffb79a056ed4bec8507fe843a35ca134353a529
Kalebe-Nascimento <kalebeinfo@gmail.com>
2025-04-23 14:57:15 -0300

    edit build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-ECR_REGISTRY="581592018732.dkr.ecr.us-east-1.amazonaws.com"
+ECR_REGISTRY="581592018732.dkr.ecr.us-east-1.amazonaws.com/"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t aws_todo .
-docker tag aws_todo:latest $ECR_REGISTRY/aws_todo:latest
-docker push $ECR_REGISTRY/aws_todo:latest
+docker build -t aws_todolist .
+docker tag aws_todolist:latest $ECR_REGISTRY/aws_todolist:latest
+docker push $ECR_REGISTRY/aws_todolist:latest
